67f2a8ff2d4d6f1861ceda91adb419ea33f7ee94
Marcin Frymark <m.frymark@alekseon.com>
2023-04-26 12:08:05 +0200

    Replace InstallData and UpgradeData with DataPatches

--- Block/WidgetForm.php
+++ Block/WidgetForm.php
@@ -218,7 +218,7 @@ class WidgetForm extends \Magento\Framework\View\Element\Template
         if ($this->tabs === null) {
             $this->tabs = [];
 
-            if ($this->getForm()->getEnableMultpipleSteps()) {
+            if ($this->getForm()->getEnableMultipleSteps()) {
                 $formTabs = $this->getForm()->getFormTabs();
                 foreach ($formTabs as $tab) {
                     $this->tabs[$tab->getId()] = $tab;
